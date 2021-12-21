43f4f69f5a09cb7adabff79e7250630f4df7250a
Rinsan <cyjdtxz@gmail.com>
2021-12-21 11:50:05 +0800

    fix: 修正在任务栏里显示名称 (#17)

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Anonymous',
       navigatorObservers: [inject<NavigatorObserver>()],
       theme: ThemeData(
         primarySwatch: Colors.blue,
